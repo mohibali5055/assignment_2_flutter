3efe47c2f73f308f18169aa49f5df50b43dba490
mohibali5055 <mohibali29j@gmail.com>
2023-06-26 07:53:29 +0500

    assignment_2_flutter

---
+++ main.dart
@@ -0,0 +1,218 @@
+
+void main() {
+  
+  //Question no 1
+  
+//   List names = ['mohib','ali','yahya','bilal','taber','haider'];
+//   print(names);
+  
+  //Question no 2
+  
+//   List<String> emptyList = [];
+  
+//   emptyList.addAll(['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']);
+  
+//   print(emptyList);
+  
+  //Question no 3
+  
+//   List days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday'];
+  
+//  int numberOfDaysToRemove = 7;
+//   int startIndex = days.length - numberOfDaysToRemove;
+//   int endIndex = days.length;
+  
+//   days.removeRange(startIndex, endIndex);
+  
+//   print(days);
+  
+  
+  //Question no 4
+  
+ 
+// List<num> numbers = [10, 5, 20, 15, 30, 25];
+
+// num smallest = numbers.reduce((curr, next) => curr < next ? curr : next);
+// num greatest = numbers.reduce((curr, next) => curr > next ? curr : next);
+
+// print('List: $numbers');
+// print('SmallestNumber: $smallest');
+// print('greatestNumber: $greatest');
+
+//Question no 5
+  
+//   Map contacts = {
+//     'bilal': 26183,
+//     'ali' : 37649,
+//     'haider': 6860,
+//     'usman' : 6291,
+//     'ravi' : 9696969,
+//     'gafe' : 86565,
+//     };
+  
+  
+//   Iterable keysLength = contacts.keys.where((key) => key.length == 4);
+  
+//   print(keysLength.toList());
+  
+  //Question no 6 
+  
+//  Map<String, Map<String, String>> world = {
+//     'Pakistan': {
+//       'capitalCity': 'Islamabad',
+//       'currency': 'rupee',
+//       'Language': 'Urdu',
+//     },
+//     'America': {
+//       'capitalCity': 'Washington, D.C.',
+//       'currency': 'USD',
+//       'Language': 'English',
+//     },
+//     'China': {
+//       'capitalCity': 'Beijing',
+//       'currency': 'yuan',
+//       'Language': 'Chinese',
+//     }
+//   };
+
+//   Map<String, String>? userValues = world['Pakistan'];
+
+//   userValues?.forEach((key, value) {
+//     print('$key: $value');
+//   });
+  
+  
+  //Question no 7
+  
+//   Map<String, double> expenses = {
+//   'sun': 3000.0,
+//   'mon': 3000.0,
+//   'tue': 3234.0,
+//   'fri' : 3525372.0
+// };
+  
+//   if(expenses.containsKey('fri')){
+//     expenses['fri'] = 5000.0;
+//   }else{
+//     expenses['fri'] = 5000.0;
+//   }
+  
+//   print(expenses);
+  
+  //Question no 8
+  
+//   List<Map<String, dynamic>> usersEligibility = [
+// {'name': 'John', 'eligible': true},
+// {'name': 'Alice', 'eligible': false},
+// {'name': 'Mike', 'eligible': true},
+// {'name': 'Sarah', 'eligible': true},
+// {'name': 'Tom', 'eligible': false},
+// ];
+  
+//   usersEligibility.removeWhere((users) => users['eligible'] == false);
+  
+//   print(usersEligibility);
+  
+  //Question no 9
+  
+//   List<int> numbers = [23,82,28,13,58,64,22,54];
+  
+//   int max = numbers.reduce((value, element) => value > element ? value:element);
+  
+//   print('greatest value in the list is $max');
+  
+  
+  //Question no 10
+  
+//     List<String> fruits = ['apple', 'orange', 'banana', 'apple', 'kiwi', 'orange'];
+
+//   List<String> uniqueStrings = fruits.toSet().toList();
+
+//   print(uniqueStrings); 
+  
+  
+  //Question no 11
+  
+  // i dont understand this question
+  
+  //Question no 12
+  
+//   List<T> getFirstN<T>(List<T> list, int n) {
+//   return list.sublist(0, n);
+// }
+  
+//   List<int> numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+//   int n = 3;
+
+//   List<int> firstNNumbers = getFirstN(numbers, n);
+
+//   print(firstNNumbers);
+  
+  
+  
+}  
+  
+  
+  
+
+  
+
+  
+  
+ 
+ 
+ 
+ 
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+
+  
+
+
+
